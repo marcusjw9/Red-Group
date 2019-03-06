129220cb063f23c32025922acbd90996b66dcf8d
marcusjw9 <48256634+marcusjw9@users.noreply.github.com>
2019-03-06 12:57:08 -0500

    Update History.java

--- History.java
+++ History.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
 
-public class historyPage {
-	private ArrayList<Event> events; 
-
+public class historyPage{
+	private ArrayList<Event> events = new ArrayList<Event>(); 
+	private ArrayList<JLabel> labels = new ArrayList<JLabel>();
+	
 	public historyPage(Task task) {
 		events = new task.getEvents();
 	}
@@ -10,10 +13,17 @@ public class historyPage {
 	public sort() {
 		
 	}
+	public Event getEvent(int index) {
+		return events[index];
+	}
 	
-	public add(Event event){
-		event.
+	public void add(Event event, JLabel j){
+		events.add(event);
+		j.setText(event.createSentence());
+		labels.add(j);
+		
 	}
 }
+
 
 
